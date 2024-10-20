3e5c99800be2ec5353eb5aab06bfa2fd7154be92
Ranmaruru <ranmaruru2@gmail.com>
2024-10-20 18:27:25 +0300

    Minor functional

--- src/main/kotlin/com/konce/service/TransactionService.kt
+++ src/main/kotlin/com/konce/service/TransactionService.kt
@@ -1,4 +1,20 @@
 package com.konce.service
 
-class TransactionService {
+import com.konce.model.Transaction
+import com.konce.repository.TransactionRepository
+import org.springframework.stereotype.Service
+import org.springframework.data.redis.core.RedisTemplate
+import java.util.UUID
+
+@Service
+class TransactionService(
+    private val transactionRepository: TransactionRepository,
+    private val redisTemplate: RedisTemplate<String, String>
+) {
+
+//    fun getTransactionsByUser(userId: UUID): List<Transaction> {
+//        val cacheKey = "transactions:$userId"
+//        val cachedTransactions = redisTemplate.opsForValue().get(cacheKey)
+//    }
+
 }
